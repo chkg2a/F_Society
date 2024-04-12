686dcaaa8ad2611901294b0b4475242ace1383bf
pkbtc <parasarkashyap06@gmail.com>
2024-04-12 18:14:45 +0530

    chnaging contarct

--- frontend/src/components/crowdfunding/Organisation.jsx
+++ frontend/src/components/crowdfunding/Organisation.jsx
@@ -4,7 +4,7 @@ const Organisation = (props) => {
     <div className="card card-compact w-96 bg-base-100 shadow-xl">
       <figure>
         <img
-          src={props.img}
+          src={props.image}
           alt="Organisation"
         />
       </figure>
