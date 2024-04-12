3ede6b8158eca8aa1713ef9428a6843fc93c4414
pkbtc <parasarkashyap06@gmail.com>
2024-04-12 15:56:09 +0530

    adding signout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -73,3 +73,11 @@ export const google=async(req,res,next)=>{
         next(error)
     }
 }
+export const signout=async(req,res,next)=>{
+    try {
+        res.clearCookie("access_token");
+        res.status(200).json({message:"You have bee sucessfully Sign out"});
+    } catch (error) {
+        next(error);
+    }
+}
--- backend/routes/auth.router.js
+++ backend/routes/auth.router.js
@@ -1,10 +1,11 @@
 import express from 'express'
 const router=express.Router();
-import { signup,signin,google } from '../controllers/auth.controller.js';
+import { signup,signin,google,signout } from '../controllers/auth.controller.js';
 
 router.post('/sign-up',signup);
 router.post('/sign-in',signin);
 router.post('/google',google);
+router.post('/sign-out',signout)
 
 
 
