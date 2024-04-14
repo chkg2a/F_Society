62a74ddaf8c84751cbbdfa8a2df5e4862b669fa1
pkbtc <parasarkashyap06@gmail.com>
2024-04-14 15:33:32 +0530

    update4

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,10 @@ app.use((err,req,res,next)=>{
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   )
-
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
 
   return res.status(statusCode).json({
     success:false,
